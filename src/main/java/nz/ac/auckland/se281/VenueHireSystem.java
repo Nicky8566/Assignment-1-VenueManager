8d20a8b2721c8023cacf33607b770ec5ec70a22a
Shadowwing1960 <98994815+Shadowwing1960@users.noreply.github.com>
2024-03-25 01:20:11 +1300

    for task 6, we can now tell how many venues there are

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -7,14 +7,16 @@ public class VenueHireSystem {
 
   //fields
   private Object[] AllVenues;
-  private Object[] Venue;
+  private String[] Venue;
   private String venueName;
   private String venueCode;
   private String capacityInput;
   private String hireFeeInput;
   private Integer NumberOfVenues = 0;
 
-  public VenueHireSystem() {}
+  public VenueHireSystem() {
+
+  }
 
   public void printVenues() {
     if (AllVenues == null) {
@@ -23,16 +25,23 @@ public class VenueHireSystem {
   }
   public boolean isInteger(String s) {
     try {
-      // converts string to integer
+      // converts string to integer, if so return true, otherwise
         Integer.parseInt(s);
         return true;
     } catch (NumberFormatException e) {
+      // if it fails, return false
         return false;
     }
 }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
+    
+        // put all the info into a array
+    Venue = new String[] {venueName, venueCode, capacityInput, hireFeeInput};
+    // put the array in a array as the first element and continue increasing the number of venues
+    AllVenues = new String[][] {Venue};
+    NumberOfVenues++;
 
     // if venue name is empty, print error message 
     if (venueName.isEmpty()) {
@@ -40,21 +49,33 @@ public class VenueHireSystem {
       return;
     }
     
-    else if(Integer.parseInt(capacityInput) < 0){
+    if(Integer.parseInt(capacityInput) < 0){
       // write error meesage for it being negative
       MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
       return;
     }
     
     // write a error meesage condition for when the string hireFeeinput isnt a interger
-    else if(isInteger(hireFeeInput) == false){
+    if(isInteger(hireFeeInput) == false){
       MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
       return;
     }
-    
-    else {
-      MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+    if(NumberOfVenues != 0){
+      String[] numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+      if (NumberOfVenues == 1){
+        MessageCli.NUMBER_VENUES.printMessage("is", numbers[NumberOfVenues - 1], "");
+      }
+      else if(NumberOfVenues < 10){
+        MessageCli.NUMBER_VENUES.printMessage("is", numbers[NumberOfVenues - 1], "s");
+      }
+      else if(NumberOfVenues > 10){
+        MessageCli.NUMBER_VENUES.printMessage("is", Integer.toString(NumberOfVenues), "s");
+      }
+      
     }
+    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
+    
+    
     
     
       
